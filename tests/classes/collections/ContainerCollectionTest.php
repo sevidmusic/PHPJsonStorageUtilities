09e162d3e89e4da07c170f3e235bbab93188bbd6
sevidmusic <sevidmusic@gmail.com>
2023-08-30 21:59:47 -0400

    Addressing issue #19

---
+++ tests/classes/collections/ContainerCollectionTest.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Darling\PHPJsonStorageUtilities\tests\classes\collections;
+
+use \Darling\PHPJsonStorageUtilities\classes\collections\ContainerCollection;
+use \Darling\PHPJsonStorageUtilities\classes\named\identifiers\Container;
+use \Darling\PHPJsonStorageUtilities\tests\PHPJsonStorageUtilitiesTest;
+use \Darling\PHPJsonStorageUtilities\tests\interfaces\collections\ContainerCollectionTestTrait;
+use \Darling\PHPTextTypes\classes\strings\ClassString;
+use \Darling\PHPTextTypes\classes\strings\Name;
+use \Darling\PHPTextTypes\classes\strings\Text;
+
+class ContainerCollectionTest extends PHPJsonStorageUtilitiesTest
+{
+
+    /**
+     * The ContainerCollectionTestTrait defines common tests for
+     * implementations of the
+     * Darling\PHPJsonStorageUtilities\interfaces\collections\ContainerCollection
+     * interface.
+     *
+     * @see ContainerCollectionTestTrait
+     *
+     */
+    use ContainerCollectionTestTrait;
+
+    public function setUp(): void
+    {
+        $this->setExpectedCollection(
+            [
+                new Container(
+                    new ClassString(Name::class),
+                ),
+                new Container(
+                    new ClassString(Text::class),
+                ),
+                new Container(
+                    new ClassString(ClassString::class),
+                ),
+                new Container(
+                    new ClassString(ContainerCollection::class),
+                ),
+                new Container(
+                    new ClassString(Container::class),
+                ),
+            ]
+        );
+        $this->setContainerCollectionTestInstance(
+            new ContainerCollection(
+                ...$this->expectedCollection(),
+            )
+        );
+    }
+
+}
+
