36f0c9ba86c6c28e55229ee6b96caa3c98455c9e
sevidmusic <sevidmusic@gmail.com>
2023-09-04 00:39:33 -0400

    Working on Draft of query logic.

--- DraftQueryLogic.php
+++ DraftQueryLogic.php
@@ -2,13 +2,14 @@
 
 include('/home/darling/Git/PHPJsonStorageUtilities/vendor/autoload.php');
 
-use \Darling\PHPJsonStorageUtilities\enumerations\Type;
-use \Darling\PHPJsonStorageUtilities\classes\filesystem\paths\JsonStorageDirectoryPath;
 use \Darling\PHPJsonStorageUtilities\classes\filesystem\paths\JsonFilePath;
+use \Darling\PHPJsonStorageUtilities\classes\filesystem\paths\JsonStorageDirectoryPath;
 use \Darling\PHPJsonStorageUtilities\classes\filesystem\storage\drivers\JsonFilesystemStorageDriver;
+use \Darling\PHPJsonStorageUtilities\classes\filesystem\storage\queries\JsonFilesystemStorageQuery;
 use \Darling\PHPJsonStorageUtilities\classes\named\identifiers\Container;
 use \Darling\PHPJsonStorageUtilities\classes\named\identifiers\Location;
 use \Darling\PHPJsonStorageUtilities\classes\named\identifiers\Owner;
+use \Darling\PHPJsonStorageUtilities\enumerations\Type;
 use \Darling\PHPJsonUtilities\classes\decoders\JsonDecoder;
 use \Darling\PHPJsonUtilities\classes\encoded\data\Json;
 use \Darling\PHPTextTypes\classes\strings\ClassString;
@@ -68,15 +69,13 @@ $name = new Name(new Text('Name'));
 
 $id = new Id();
 
-var_dump(
-    $jsonFilesystemStorageDriver->write(
-        $json,
-        $jsonStorageDirectoryPath,
-        $location,
-        $owner,
-        $name,
-        $id
-    )
+$jsonFilesystemStorageDriver->write(
+    $json,
+    $jsonStorageDirectoryPath,
+    $location,
+    $owner,
+    $name,
+    $id
 );
 
 $jsonFilePath = new JsonFilePath(
@@ -88,21 +87,36 @@ $jsonFilePath = new JsonFilePath(
         $id
 );
 
-var_dump(file_get_contents($jsonFilePath->__toString()));
+# var_dump(file_get_contents($jsonFilePath->__toString()));
 
-$query = new \Darling\PHPJsonStorageUtilities\classes\filesystem\storage\queries\JsonFilesystemStorageQuery(jsonStorageDirectoryPath: $jsonStorageDirectoryPath);
+$query = new JsonFilesystemStorageQuery(
+    #jsonFilePath: $jsonFilePath,
+    jsonStorageDirectoryPath: $jsonStorageDirectoryPath,
+    location: $location,
+    container: $container,
+    owner: $owner,
+    name: $name,
+    #id: $id,
+);
 
-$files = glob(
-        $query->jsonStorageDirectoryPath()->__toString() .
-        DIRECTORY_SEPARATOR . '*'.
-        DIRECTORY_SEPARATOR . '*'.
-        DIRECTORY_SEPARATOR . '*'.
-        DIRECTORY_SEPARATOR . '*'.
-        DIRECTORY_SEPARATOR . '*'.
-        DIRECTORY_SEPARATOR . '*'
-    );
+$globString =
+    (is_null($query->jsonStorageDirectoryPath()) ? dirname($jsonStorageDirectoryPath) . DIRECTORY_SEPARATOR . '*': $query->jsonStorageDirectoryPath()) .
+    DIRECTORY_SEPARATOR .
+    (is_null($query->location()) ? '*' : $query->location()) .
+    DIRECTORY_SEPARATOR .
+    (is_null($query->container()) ? '*' : $query->container()) .
+    DIRECTORY_SEPARATOR .
+    (is_null($query->owner()) ? '*' : $query->owner()) .
+    DIRECTORY_SEPARATOR .
+    (is_null($query->name()) ? '*' : $query->name()) .
+    DIRECTORY_SEPARATOR .
+    (is_null($query->id()) ? '*' : $query->id());
 
-foreach($files as $file) {
-    echo $file . PHP_EOL;
-}
 
+
+
+echo $globString;
+
+$files = glob($globString);
+
+# var_dump($files);
