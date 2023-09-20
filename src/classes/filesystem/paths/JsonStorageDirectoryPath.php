7ce0306b735a10900e027091f29f9211fad06c0a
sevidmusic <sevidmusic@gmail.com>
2023-09-20 18:09:31 -0400

    Addressing issue #29

--- src/classes/filesystem/paths/JsonStorageDirectoryPath.php
+++ src/classes/filesystem/paths/JsonStorageDirectoryPath.php
@@ -8,6 +8,13 @@ use \Darling\PHPTextTypes\interfaces\strings\Name;
 final class JsonStorageDirectoryPath implements JsonStorageDirectoryPathInterface
 {
 
+    private const HOME_DIRECTORY_PATH = 'dir';
+    private const LOCAL_DIRECTORY_NAME = '.local';
+    private const SHARE_DIRECTORY_NAME = 'share';
+    private const DARLING_DIRECTORY_NAME = 'darling';
+    private const DATA_DIRECTORY_NAME = 'data';
+    private const TMP_DIRECTORY_PATH = DIRECTORY_SEPARATOR . 'tmp';
+
     /**
      * Instantiate a new instance of a JsonStorageDirectoryPath.
      *
@@ -23,14 +30,25 @@ final class JsonStorageDirectoryPath implements JsonStorageDirectoryPathInterfac
 
     public function __toString(): string
     {
+        return $this->dataDirectoryPath() .
+            DIRECTORY_SEPARATOR .
+            $this->name()->__toString();
+    }
+
+    private function dataDirectoryPath(): string
+    {
         $userInfo = posix_getpwuid(posix_geteuid());
-        if(is_array($userInfo) && isset($userInfo['dir'])) {
+        if(
+            is_array($userInfo)
+            &&
+            isset($userInfo[self::HOME_DIRECTORY_PATH])
+        ) {
             $storageDirectoryPath = realpath(
-                $userInfo['dir'] .
+                $userInfo[self::HOME_DIRECTORY_PATH] .
                 DIRECTORY_SEPARATOR .
-                '.local' .
+                self::LOCAL_DIRECTORY_NAME .
                 DIRECTORY_SEPARATOR .
-                'share'
+                self::SHARE_DIRECTORY_NAME
             );
         }
         return (
@@ -40,15 +58,12 @@ final class JsonStorageDirectoryPath implements JsonStorageDirectoryPathInterfac
             &&
             is_writable($storageDirectoryPath)
             ? $storageDirectoryPath
-            : DIRECTORY_SEPARATOR . 'tmp'
+            : self::TMP_DIRECTORY_PATH
         ) .
-        DIRECTORY_SEPARATOR .
-        'darling' .
-        DIRECTORY_SEPARATOR .
-        'data' .
-        DIRECTORY_SEPARATOR .
-        $this->name()->__toString();
+            DIRECTORY_SEPARATOR .
+            self::DARLING_DIRECTORY_NAME .
+            DIRECTORY_SEPARATOR .
+            self::DATA_DIRECTORY_NAME;
     }
-
 }
 
