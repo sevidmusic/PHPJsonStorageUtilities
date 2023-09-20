678fbb5bf139a4b7383566b3d5c3949893d84108
sevidmusic <sevidmusic@gmail.com>
2023-09-20 10:31:46 -0400

    Working on addressing issue #29

--- src/classes/named/identifiers/Location.php
+++ src/classes/named/identifiers/Location.php
@@ -5,11 +5,6 @@ namespace Darling\PHPJsonStorageUtilities\classes\named\identifiers;
 use \Darling\PHPJsonStorageUtilities\classes\named\identifiers\NamedIdentifier;
 use \Darling\PHPJsonStorageUtilities\interfaces\named\identifiers\Location as LocationInterface;
 
-/**
- * A Location is one part of a JsonFilePath that determines where
- * json is stored.
- *
- */
 final class Location extends NamedIdentifier implements LocationInterface
 {
 
