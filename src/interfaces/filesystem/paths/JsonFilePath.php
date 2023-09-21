3df68f56d2d1add92f530becbf00090235ff970f
sevidmusic <sevidmusic@gmail.com>
2023-09-21 15:23:41 -0400

    Working on adressing issue #29

--- src/interfaces/filesystem/paths/JsonFilePath.php
+++ src/interfaces/filesystem/paths/JsonFilePath.php
@@ -11,7 +11,7 @@ use \Stringable;
 
 /**
  * A JsonFilePath can be used to construct an appropriate path to a
- * json file.
+ * json file in a storage directory.
  *
  * The complete path to the json file can be obtained via the
  * __toString() method.
@@ -30,6 +30,7 @@ use \Stringable;
  * $this->name()->__toString() .
  * DIRECTORY_SEPARATOR .
  * # shardId() is not public, yet, see issue #31.
+ * # https://github.com/sevidmusic/PHPJsonStorageUtilities/issues/31
  * $this->shardId($this->id()) .
  * '.json';
  *
@@ -38,12 +39,86 @@ use \Stringable;
  */
 interface JsonFilePath extends \Stringable
 {
+
+    /**
+     * Return an instance of a JsonStorageDirectoryPath.
+     *
+     * @return JsonStorageDirectoryPath
+     *
+     */
     public function jsonStorageDirectoryPath(): JsonStorageDirectoryPath;
+
+
+    /**
+     * Return an instance of a Location.
+     *
+     * @return Location
+     *
+     */
     public function location(): Location;
+
+
+    /**
+     * Return an instance of a Container.
+     *
+     * @return Container
+     *
+     */
     public function container(): Container;
+
+
+    /**
+     * Return an instance of an Owner.
+     *
+     * @return Owner
+     *
+     */
     public function owner(): Owner;
+
+
+    /**
+     * Return an instance of a Name.
+     *
+     * @return Name
+     *
+     */
     public function name(): Name;
+
+
+    /**
+     * Return an instance of an Id.
+     *
+     * @return Id
+     *
+     */
     public function id(): Id;
+
+    /**
+     * Return the complete json file path.
+     *
+     * The json file path will be constructed as follows:
+     *
+     * ```
+     * $this->jsonStorageDirectoryPath()->__toString() .
+     * DIRECTORY_SEPARATOR .
+     * $this->location()->__toString() .
+     * DIRECTORY_SEPARATOR .
+     * $this->container()->__toString() .
+     * DIRECTORY_SEPARATOR .
+     * $this->owner()->__toString() .
+     * DIRECTORY_SEPARATOR .
+     * $this->name()->__toString() .
+     * DIRECTORY_SEPARATOR .
+     * # shardId() is not public, yet, see issue #31.
+     * # https://github.com/sevidmusic/PHPJsonStorageUtilities/issues/31
+     * $this->shardId($this->id()) .
+     * '.json';
+     *
+     * ```
+     *
+     * @return string
+     *
+     */
     public function __toString(): string;
 }
 
