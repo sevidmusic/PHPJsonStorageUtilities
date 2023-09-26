7b680d31076ceb8a783a6aeaa65e5fa4c0cd5cd5
sevidmusic <sevidmusic@gmail.com>
2023-09-26 16:06:30 -0400

    Addressing issue #29

---
+++ examples/NameCollectionExample.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Darling\PHPJsonStorageUtilities\examples;
+
+include(dirname(__DIR__, 1) . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php');
+
+/**
+ * This file demonstrates the usage of a NameCollection.
+ */
+
+
