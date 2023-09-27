eac16284a8c92313a1a5c5f3dd7c58d86243d0cc
sevidmusic <sevidmusic@gmail.com>
2023-09-27 00:55:13 -0400

    Addressing issue #29

--- examples/IdCollectionExample.php
+++ examples/IdCollectionExample.php
@@ -4,11 +4,28 @@ namespace Darling\PHPJsonStorageUtilities\examples;
 
 include(dirname(__DIR__, 1) . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php');
 
-use \Darling\PHPJsonStorageUtilities\classes\collections\JsonCollection;
+use \Darling\PHPJsonStorageUtilities\classes\collections\IdCollection;
+use \Darling\PHPTextTypes\classes\strings\Id;
+use \Darling\PHPJsonStorageUtilities\tests\IntegrationTestUtilities;
 
 /**
  * This file demonstrates the usage of a IdCollection.
  */
 
-$jsonCollection = new JsonCollection();
+$idCollection = new IdCollection(
+    new Id(),
+    new Id(),
+    new Id(),
+    new Id(),
+    new Id(),
+    new Id(),
+);
+
+foreach($idCollection->collection() as $index => $id) {
+    echo PHP_EOL . 'Id ' . strval($index) . ':' .
+        IntegrationTestUtilities::applyANSIColor(
+            $id->__toString(),
+            rand(1, 231)
+        );
+}
 
