03e00d445d20e395ae9348755c0db844a6f201cd
sevidmusic <sevidmusic@gmail.com>
2023-09-27 09:28:32 -0400

    Addressing issue #29

---
+++ examples/NameCollectionExample.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Darling\PHPJsonStorageUtilities\examples;
+
+include(dirname(__DIR__, 1) . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php');
+
+use \Darling\PHPJsonStorageUtilities\classes\collections\NameCollection;
+use \Darling\PHPJsonStorageUtilities\tests\IntegrationTestUtilities;
+use \Darling\PHPTextTypes\classes\strings\Name;
+use \Darling\PHPTextTypes\classes\strings\Text;
+
+/**
+ * This file demonstrates the usage of a NameCollection.
+ */
+
+$nameCollection = new NameCollection(
+    new Name(new Text('Name' . strval(rand(1, 100)))),
+    new Name(new Text('Name' . strval(rand(1, 100)))),
+    new Name(new Text('Name' . strval(rand(1, 100)))),
+    new Name(new Text('Name' . strval(rand(1, 100)))),
+    new Name(new Text('Name' . strval(rand(1, 100)))),
+    new Name(new Text('Name' . strval(rand(1, 100)))),
+);
+
+foreach($nameCollection->collection() as $index => $name) {
+    echo PHP_EOL . 'Name ' . strval($index) . ':' .
+        IntegrationTestUtilities::applyANSIColor(
+            $name->__toString(),
+            rand(1, 231)
+        );
+}
+
