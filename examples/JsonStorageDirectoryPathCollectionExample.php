1cf8738ca0a020d241f781f338f06364318d9fcf
sevidmusic <sevidmusic@gmail.com>
2023-09-27 01:10:10 -0400

    Addressing issue #29

--- examples/JsonStorageDirectoryPathCollectionExample.php
+++ examples/JsonStorageDirectoryPathCollectionExample.php
@@ -1,14 +1,48 @@
 <?php
 
-namespace Darling\PHPJsonStorageUtilities\examples;
+namespace Darling\PHPJsonStorageDirectoryPathStorageUtilities\examples;
 
 include(dirname(__DIR__, 1) . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php');
 
-use \Darling\PHPJsonStorageUtilities\classes\filesystem\paths\JsonFilePath;
+use Darling\PHPJsonStorageUtilities\classes\filesystem\paths\JsonStorageDirectoryPath;
+use \Darling\PHPJsonStorageUtilities\classes\collections\JsonStorageDirectoryPathCollection;
+use \Darling\PHPTextTypes\classes\strings\Name;
+use \Darling\PHPTextTypes\classes\strings\Text;
+use \Darling\PHPJsonStorageUtilities\tests\IntegrationTestUtilities;
 
 /**
  * This file demonstrates the usage of a JsonStorageDirectoryPathCollection.
  */
 
-$jsonFilePath = new JsonFilePath();
+$jsonStorageDirectoryPathCollection = new JsonStorageDirectoryPathCollection(
+    new JsonStorageDirectoryPath(
+        new Name(new Text('Name' . strval(rand(1, 100))))
+    ),
+    new JsonStorageDirectoryPath(
+        new Name(new Text('Name' . strval(rand(1, 100))))
+    ),
+    new JsonStorageDirectoryPath(
+        new Name(new Text('Name' . strval(rand(1, 100))))
+    ),
+    new JsonStorageDirectoryPath(
+        new Name(new Text('Name' . strval(rand(1, 100))))
+    ),
+    new JsonStorageDirectoryPath(
+        new Name(new Text('Name' . strval(rand(1, 100))))
+    ),
+    new JsonStorageDirectoryPath(
+        new Name(new Text('Name' . strval(rand(1, 100))))
+    ),
+    new JsonStorageDirectoryPath(
+        new Name(new Text('Name' . strval(rand(1, 100))))
+    ),
+);
+
+foreach($jsonStorageDirectoryPathCollection->collection() as $index => $jsonStorageDirectoryPath) {
+    echo PHP_EOL . 'JsonStorageDirectoryPath ' . strval($index) . ':' .
+        IntegrationTestUtilities::applyANSIColor(
+            $jsonStorageDirectoryPath->__toString(),
+            rand(1, 231)
+        );
+}
 
