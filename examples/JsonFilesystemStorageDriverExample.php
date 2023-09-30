f9c1664a83a3a228f9f0f741b7d965b990a80638
sevidmusic <sevidmusic@gmail.com>
2023-09-30 14:40:19 -0400

    Addressing issue #29

--- examples/JsonFilesystemStorageDriverExample.php
+++ examples/JsonFilesystemStorageDriverExample.php
@@ -4,7 +4,6 @@ namespace Darling\PHPJsonStorageUtilities\examples;
 
 include(dirname(__DIR__, 1) . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php');
 
-use Darling\PHPJsonStorageUtilities\enumerations\Type;
 use \Darling\PHPJsonStorageUtilities\classes\filesystem\paths\JsonFilePath;
 use \Darling\PHPJsonStorageUtilities\classes\filesystem\paths\JsonStorageDirectoryPath;
 use \Darling\PHPJsonStorageUtilities\classes\filesystem\storage\drivers\JsonFilesystemStorageDriver;
@@ -17,38 +16,85 @@ use \Darling\PHPJsonUtilities\classes\encoded\data\Json;
 use \Darling\PHPTextTypes\classes\strings\Id;
 use \Darling\PHPTextTypes\classes\strings\Name;
 use \Darling\PHPTextTypes\classes\strings\Text;
+use stdClass;
 
-// Instantiate a new JsonFilesystemStorageDriver
+/**
+ * Instantiate a new JsonFilesystemStorageDriver
+ */
 $jsonFilesystemStorageDriver = new JsonFilesystemStorageDriver();
 
-// Create some data to store.
-$data = [new Id(), 'Foo' . strval(rand(1, 100)), rand(PHP_INT_MIN, PHP_INT_MAX)];
+/**
+ * Create an array of data.
+ */
+$data = [
+    new Id(),
+    'Foo' . strval(rand(1, 100)),
+    rand(PHP_INT_MIN, PHP_INT_MAX),
+    function () : void {},
+    new stdClass(),
+    new Json('Encoded Json Instance' . strval(rand(1, 100))),
+    json_encode('json string ' . strval(rand(1, 100))),
+    floatval(
+        strval(rand(0, 100)) .
+        '.' .
+        strval(rand(1, 100))
+    ),
+    [
+        "Id" => new Id(),
+        "string" => 'Foo' . strval(rand(1, 100)),
+        "int" => rand(PHP_INT_MIN, PHP_INT_MAX),
+        "closure" => function () : void {},
+        "object" => new stdClass(),
+        "Json Instance" => new Json('Encoded Json Instance' . strval(rand(1, 100))),
+        "json string" => json_encode('json string ' . strval(rand(1, 100))),
+        "float" => floatval(
+            strval(rand(0, 100)) .
+            '.' .
+            strval(rand(1, 100))
+        ),
+    ],
+];
 
-// Encode the data as json
-$json = new Json($data);
+/**
+ * Randomly select some data from the $data array and encode
+ * it as Json.
+ */
+$json = new Json($data[array_rand($data)]);
 
-// Instantiate a new JsonFilePath. The JsonFilePath will determine
-// the path where the json will be written to.
+/**
+ * Instantiate a new JsonFilePath to emulate the path to the json file
+ * that the Json is expected to be written to. This will be used to
+ * query storage later.
+ */
+
 $expectedJsonFilePath = new JsonFilePath(
     jsonStorageDirectoryPath: new JsonStorageDirectoryPath(
-        new Name(new Text('JsonStorageDirectoryName' . strval(rand(1, 100))))
+        new Name(
+            new Text(
+                'JsonStorageDirectoryName' . strval(rand(1, 100))
+            )
+        )
     ),
-    location: new Location(new Name(new Text('Location'))),
+    location: new Location(
+        new Name(new Text('Location' . strval(rand(1, 100))))
+    ),
     container: new Container(
-        /**
+        /***
          * In the future Container::determineType(Json $json) should
          * be used to determine Type for Container.
          *
-         * @see https://github.com/sevidmusic/PHPJsonStorageUtilities/issues/34
+         * @see https:*github.com/sevidmusic/PHPJsonStorageUtilities/issues/34
          */
-        Type::Array,
+        IntegrationTestUtilities::determineType($json),
     ),
-    owner: new Owner(new Name(new Text('Owner'))),
-    name: new Name(new Text('Name')),
+    owner: new Owner(new Name(new Text('Owner' . strval(rand(1, 100))))),
+    name: new Name(new Text('Name' . strval(rand(1, 100)))),
     id: new Id(),
 );
 
-// Write the json to storage
+/**
+ * Write the json to storage
+ */
 $jsonFilesystemStorageDriver->write(
     $json,
     $expectedJsonFilePath->jsonStorageDirectoryPath(),
@@ -58,21 +104,49 @@ $jsonFilesystemStorageDriver->write(
     $expectedJsonFilePath->id(),
 );
 
-// Instantiate a JsonFilesystemStorageQuery that will be
-// used to read the json that was stored on the call to
-// $jsonFilesystemStorageDriver->write(...)
+/**
+ * Instantiate a JsonFilesystemStorageQuery that will be passed to
+ * the $jsonFilesystemStorageDriver->read() method to query storage.
+ * In this example the JsonFilesystemStorageQuery will only define a
+ * single query parameter: jsonFilePath
+ *
+ * Though any combination of the following parameters is possible:
+ *
+ * @param JsonFilePath|null $jsonFilePath
+ * @param JsonStorageDirectoryPath|null $jsonStorageDirectoryPath
+ * @param Location|null $location
+ * @param Container|null $container
+ * @param Owner|null $owner
+ * @param Name|null $name
+ * @param Id|null $id
+ *
+ * If defined, the jsonFilePath parameter will always take precedence
+ * over the other parameters.
+ */
 $jsonFilesystemStorageQuery = new JsonFilesystemStorageQuery(
     jsonFilePath: $expectedJsonFilePath
 );
 
-// Get a JsonCollection of the Json in storage that
-// matches the specified $jsonFilesystemStorageQuery
+/**
+ * Get a JsonCollection of the Json in storage that
+ * matches the specified JsonFilesystemStorageQuery.
+ *
+ * The JsonFilesystemStorageQuery in this example defines a single
+ * query parameter: jsonFilePath.
+ *
+ * If a json file exists at the path that matches the JsonFilePath
+ * specified  by the JsonFilesystemStorageQuery, then it's Json will
+ * be the only Json included in the JsonCollection returned by read().
+ *
+ */
 $jsonCollection = $jsonFilesystemStorageDriver->read(
     $jsonFilesystemStorageQuery
 );
 
-// echo any stored json that matched the JsonFilesystemStorageQuery
-// that was passed to $jsonFilesystemStorageDriver->read()
+/**
+ * Echo any stored json that matched the JsonFilesystemStorageQuery
+ * that was passed to $jsonFilesystemStorageDriver->read()
+ */
 foreach($jsonCollection->collection() as $index => $json) {
     echo PHP_EOL . 'Json ' . strval($index) . ':' .
         IntegrationTestUtilities::applyANSIColor(
@@ -81,14 +155,18 @@ foreach($jsonCollection->collection() as $index => $json) {
         );
 }
 
-// Instantiate a JsonFilesystemStorageQuery that will be used to
-// delete the entire JsonStorageDirectory that the json was written
-// to on the call to  $jsonFilesystemStorageDriver->write(...)
+/**
+ * Instantiate a JsonFilesystemStorageQuery that will be used to
+ * delete the entire JsonStorageDirectory that the json was written
+ * to on the call to  $jsonFilesystemStorageDriver->write(...)
+ */
 $jsonFilesystemStorageQuery = new JsonFilesystemStorageQuery(
     jsonStorageDirectoryPath: $expectedJsonFilePath->jsonStorageDirectoryPath()
 );
 
-// Delete the JsonStorageDirectory that was used in this example.
+/**
+ * Delete the JsonStorageDirectory that was used in this example.
+ */
 $jsonFilesystemStorageDriver->delete(
     $jsonFilesystemStorageQuery
 );
