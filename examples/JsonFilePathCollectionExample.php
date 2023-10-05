4d5402036f0d8a4f2badf435c6823c6c771d244b
sevidmusic <sevidmusic@gmail.com>
2023-10-05 15:25:49 -0400

    Addressing issue #29

--- examples/JsonFilePathCollectionExample.php
+++ examples/JsonFilePathCollectionExample.php
@@ -28,80 +28,160 @@ use \Darling\PHPTextTypes\classes\strings\Text;
 
 $jsonFilePathCollection = new JsonFilePathCollection(
     new JsonFilePath(
-        new JsonStorageDirectoryPath(new Name(new Text('StorageDirectoryPathName' . strval(rand(1, 100))))),
-        new Location(new Name(new Text('Location' . strval(rand(1, 100))))),
+        new JsonStorageDirectoryPath(
+            new Name(
+                new Text(
+                    'StorageDirectoryPathName' . strval(rand(1, 100))
+                )
+            )
+        ),
+        new Location(
+            new Name(new Text('Location' . strval(rand(1, 100))))
+        ),
         new Container(Type::Array),
         new Owner(new Name(new Text('Owner' . strval(rand(1, 100))))),
         new Name(new Text('Name' . strval(rand(1, 100)))),
         new Id(),
     ),
     new JsonFilePath(
-        new JsonStorageDirectoryPath(new Name(new Text('StorageDirectoryPathName' . strval(rand(1, 100))))),
-        new Location(new Name(new Text('Location' . strval(rand(1, 100))))),
+        new JsonStorageDirectoryPath(
+            new Name(
+                new Text(
+                    'StorageDirectoryPathName' . strval(rand(1, 100))
+                )
+            )
+        ),
+        new Location(
+            new Name(new Text('Location' . strval(rand(1, 100))))
+        ),
         new Container(Type::Bool),
         new Owner(new Name(new Text('Owner' . strval(rand(1, 100))))),
         new Name(new Text('Name' . strval(rand(1, 100)))),
         new Id(),
     ),
     new JsonFilePath(
-        new JsonStorageDirectoryPath(new Name(new Text('StorageDirectoryPathName' . strval(rand(1, 100))))),
-        new Location(new Name(new Text('Location' . strval(rand(1, 100))))),
+        new JsonStorageDirectoryPath(
+            new Name(
+                new Text(
+                    'StorageDirectoryPathName' . strval(rand(1, 100))
+                )
+            )
+        ),
+        new Location(
+            new Name(new Text('Location' . strval(rand(1, 100))))
+        ),
         new Container(Type::Float),
         new Owner(new Name(new Text('Owner' . strval(rand(1, 100))))),
         new Name(new Text('Name' . strval(rand(1, 100)))),
         new Id(),
     ),
     new JsonFilePath(
-        new JsonStorageDirectoryPath(new Name(new Text('StorageDirectoryPathName' . strval(rand(1, 100))))),
-        new Location(new Name(new Text('Location' . strval(rand(1, 100))))),
+        new JsonStorageDirectoryPath(
+            new Name(
+                new Text(
+                    'StorageDirectoryPathName' . strval(rand(1, 100))
+                )
+            )
+        ),
+        new Location(
+            new Name(new Text('Location' . strval(rand(1, 100))))
+        ),
         new Container(Type::Int),
         new Owner(new Name(new Text('Owner' . strval(rand(1, 100))))),
         new Name(new Text('Name' . strval(rand(1, 100)))),
         new Id(),
     ),
     new JsonFilePath(
-        new JsonStorageDirectoryPath(new Name(new Text('StorageDirectoryPathName' . strval(rand(1, 100))))),
-        new Location(new Name(new Text('Location' . strval(rand(1, 100))))),
+        new JsonStorageDirectoryPath(
+            new Name(
+                new Text(
+                    'StorageDirectoryPathName' . strval(rand(1, 100))
+                )
+            )
+        ),
+        new Location(
+            new Name(new Text('Location' . strval(rand(1, 100))))
+        ),
         new Container(Type::Null),
         new Owner(new Name(new Text('Owner' . strval(rand(1, 100))))),
         new Name(new Text('Name' . strval(rand(1, 100)))),
         new Id(),
     ),
     new JsonFilePath(
-        new JsonStorageDirectoryPath(new Name(new Text('StorageDirectoryPathName' . strval(rand(1, 100))))),
-        new Location(new Name(new Text('Location' . strval(rand(1, 100))))),
+        new JsonStorageDirectoryPath(
+            new Name(
+                new Text(
+                    'StorageDirectoryPathName' . strval(rand(1, 100))
+                )
+            )
+        ),
+        new Location(
+            new Name(new Text('Location' . strval(rand(1, 100))))
+        ),
         new Container(Type::Object),
         new Owner(new Name(new Text('Owner' . strval(rand(1, 100))))),
         new Name(new Text('Name' . strval(rand(1, 100)))),
         new Id(),
     ),
     new JsonFilePath(
-        new JsonStorageDirectoryPath(new Name(new Text('StorageDirectoryPathName' . strval(rand(1, 100))))),
-        new Location(new Name(new Text('Location' . strval(rand(1, 100))))),
+        new JsonStorageDirectoryPath(
+            new Name(
+                new Text(
+                    'StorageDirectoryPathName' . strval(rand(1, 100))
+                )
+            )
+        ),
+        new Location(
+            new Name(new Text('Location' . strval(rand(1, 100))))
+        ),
         new Container(Type::Resource),
         new Owner(new Name(new Text('Owner' . strval(rand(1, 100))))),
         new Name(new Text('Name' . strval(rand(1, 100)))),
         new Id(),
     ),
     new JsonFilePath(
-        new JsonStorageDirectoryPath(new Name(new Text('StorageDirectoryPathName' . strval(rand(1, 100))))),
-        new Location(new Name(new Text('Location' . strval(rand(1, 100))))),
+        new JsonStorageDirectoryPath(
+            new Name(
+                new Text(
+                    'StorageDirectoryPathName' . strval(rand(1, 100))
+                )
+            )
+        ),
+        new Location(
+            new Name(new Text('Location' . strval(rand(1, 100))))
+        ),
         new Container(Type::ResourceClosed),
         new Owner(new Name(new Text('Owner' . strval(rand(1, 100))))),
         new Name(new Text('Name' . strval(rand(1, 100)))),
         new Id(),
     ),
     new JsonFilePath(
-        new JsonStorageDirectoryPath(new Name(new Text('StorageDirectoryPathName' . strval(rand(1, 100))))),
-        new Location(new Name(new Text('Location' . strval(rand(1, 100))))),
+        new JsonStorageDirectoryPath(
+            new Name(
+                new Text(
+                    'StorageDirectoryPathName' . strval(rand(1, 100))
+                )
+            )
+        ),
+        new Location(
+            new Name(new Text('Location' . strval(rand(1, 100))))
+        ),
         new Container(Type::String),
         new Owner(new Name(new Text('Owner' . strval(rand(1, 100))))),
         new Name(new Text('Name' . strval(rand(1, 100)))),
         new Id(),
     ),
     new JsonFilePath(
-        new JsonStorageDirectoryPath(new Name(new Text('StorageDirectoryPathName' . strval(rand(1, 100))))),
-        new Location(new Name(new Text('Location' . strval(rand(1, 100))))),
+        new JsonStorageDirectoryPath(
+            new Name(
+                new Text(
+                    'StorageDirectoryPathName' . strval(rand(1, 100))
+                )
+            )
+        ),
+        new Location(
+            new Name(new Text('Location' . strval(rand(1, 100))))
+        ),
         new Container(Type::UnknownType),
         new Owner(new Name(new Text('Owner' . strval(rand(1, 100))))),
         new Name(new Text('Name' . strval(rand(1, 100)))),
@@ -109,7 +189,9 @@ $jsonFilePathCollection = new JsonFilePathCollection(
     ),
 );
 
-foreach($jsonFilePathCollection->collection() as $index => $jsonFilePath) {
+foreach(
+    $jsonFilePathCollection->collection() as $index => $jsonFilePath
+) {
     echo PHP_EOL . 'JsonFilePath[' . strval($index) . ']: ' .
         IntegrationTestUtilities::applyANSIColor(
             $jsonFilePath->__toString(),
